cf914679b153a8e8e762671e9c6e836ff22dfe29
Vincent Garnier <forxer@gmail.com>
2014-11-21 11:21:17 +0100

    small code quality enhancements

--- src/Tao/Provider/SupportServiceProvider.php
+++ src/Tao/Provider/SupportServiceProvider.php
@@ -8,14 +8,14 @@ use Tao\Support\StopWords;
 
 class SupportServiceProvider implements ServiceProviderInterface
 {
-	public function register(Container $app)
-	{
-		$app['bourinator'] = $app->factory(function() use ($app) {
-			return new Bourinator();
-		});
+    public function register(Container $app)
+    {
+        $app['bourinator'] = $app->factory(function() {
+            return new Bourinator();
+        });
 
-		$app['stopwords'] = function() use ($app) {
-			return new StopWords();
-		};
-	}
+        $app['stopwords'] = function() {
+            return new StopWords();
+        };
+    }
 }
