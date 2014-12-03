1327ea90fdec024914458f3797f481b112e20435
Vincent Garnier <forxer@gmail.com>
2014-12-03 22:23:14 +0100

    try to move controller resolver

--- src/Tao/Application.php
+++ src/Tao/Application.php
@@ -21,122 +21,166 @@ use Whoops\Handler\PrettyPageHandler as WhoopsHandler;
 
 abstract class Application extends Container
 {
-	public $utilities;
+    public $utilities;
 
-	public $class;
+    public $class;
 
-	protected static $models;
+    protected static $models;
 
-	/**
-	 * Application constructor.
-	 *
-	 * @param object $loader The autoloader instance.
-	 * @param array $config The custom configuration of the application.
-	 * @param string $appPath The application absolute path.
-	 * @param array $classesMap The custom classes map of the application.
-	 */
-	public function __construct($loader, array $config = [], $appPath = null, array $classesMap = [])
-	{
-		# Utilities
-		$this->utilities = new Utilities($this);
+    /**
+     * Application constructor.
+     *
+     * @param object $loader The autoloader instance.
+     * @param array $config The custom configuration of the application.
+     * @param string $appPath The application absolute path.
+     * @param array $classesMap The custom classes map of the application.
+     */
+    public function __construct($loader, array $config = [], $appPath = null, array $classesMap = [])
+    {
+        # Utilities
+        $this->utilities = new Utilities($this);
 
-		# Register start time
-		$this->utilities->registerStartTime();
+        # Register start time
+        $this->utilities->registerStartTime();
 
-		# Store application path
-		$this->utilities->setApplicationPath($appPath);
+        # Store application path
+        $this->utilities->setApplicationPath($appPath);
 
-		# Store classes map
-		$this['class'] = $this->utilities->setClassesMap($classesMap);
+        # Store classes map
+        $this['class'] = $this->utilities->setClassesMap($classesMap);
 
-		# Call container constructor
-		parent::__construct($this->utilities->setConfiguration($config));
+        # Call container constructor
+        parent::__construct($this->utilities->setConfiguration($config));
 
-		# Register core services providers
-		$this->register(new FilesystemServiceProvider());
-		$this->register(new FinderServiceProvider());
-		$this->register(new HttpServiceProvider());
-		$this->register(new LoggerServiceProvider());
-		$this->register(new MessagesServiceProvider());
-		$this->register(new RouterServiceProvider());
-		$this->register(new SupportServiceProvider());
-		$this->register(new TemplatingServiceProvider());
-		$this->register(new TriggersServiceProvider());
+        # Register core services providers
+        $this->register(new FilesystemServiceProvider());
+        $this->register(new FinderServiceProvider());
+        $this->register(new HttpServiceProvider());
+        $this->register(new LoggerServiceProvider());
+        $this->register(new MessagesServiceProvider());
+        $this->register(new RouterServiceProvider());
+        $this->register(new SupportServiceProvider());
+        $this->register(new TemplatingServiceProvider());
+        $this->register(new TriggersServiceProvider());
 
-		# Enables the portablity layer and configures PHP for UTF-8
-		Utf8Bootup::initAll();
+        # Enables the portablity layer and configures PHP for UTF-8
+        Utf8Bootup::initAll();
 
-		# Redirects to an UTF-8 encoded URL if it's not already the case
-		Utf8Bootup::filterRequestUri();
+        # Redirects to an UTF-8 encoded URL if it's not already the case
+        Utf8Bootup::filterRequestUri();
 
-		# Normalizes HTTP inputs to UTF-8 NFC
-		Utf8Bootup::filterRequestInputs();
+        # Normalizes HTTP inputs to UTF-8 NFC
+        Utf8Bootup::filterRequestInputs();
 
-		# Print errors in debug mode
-		if ($this['debug'])
-		{
-			$whoops = new WhoopsRun;
-			$whoops->pushHandler(new WhoopsHandler);
-			$whoops->register();
-		}
-		# otherwise log them
-		else {
-			ErrorHandler::register($this['phpLogger']);
-		}
+        # Print errors in debug mode
+        if ($this['debug'])
+        {
+            $whoops = new WhoopsRun;
+            $whoops->pushHandler(new WhoopsHandler);
+            $whoops->register();
+        }
+        # otherwise log them
+        else {
+            ErrorHandler::register($this['phpLogger']);
+        }
 
-		# Only enable Kint in debug mode
-		\Kint::enabled($this['debug']);
-	}
+        # Only enable Kint in debug mode
+        \Kint::enabled($this['debug']);
+    }
 
-	/**
-	 * Run the application.
-	 *
-	 */
-	public function run()
-	{
-		try
-		{
-			$this['request']->attributes->add(
-				$this['router']->matchRequest($this['request'])
-			);
+    /**
+     * Run the application.
+     *
+     */
+    public function run()
+    {
+        try
+        {
+            $this['triggers']->callTrigger('before-match-request', $this);
 
-			$response = $this['controllerResolver'];
-		}
-		catch (ResourceNotFoundException $e)
-		{
-			$response = (new Controller($this))->serve404();
-		}
-		catch (\Exception $e)
-		{
-			$response = new Response();
-			$response->headers->set('Content-Type', 'text/plain');
-			$response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
-			$response->setContent($e->getMessage());
-		}
+            $this['request']->attributes->add(
+                $this['router']->matchRequest($this['request'])
+            );
 
-		if ($this['x-frame-options']) {
-			$response->headers->set('x-frame-options', $this['x-frame-options']);
-		}
+            $this['triggers']->callTrigger('before-controller-resolver', $this);
 
-		$response->prepare($this['request']);
+//            $response = $this['controllerResolver'];
+            $response = $this->controllerResolver();
+        }
+        catch (ResourceNotFoundException $e)
+        {
+            $response = (new Controller($this))->serve404();
+        }
+        /*
+        catch (\Exception $e)
+        {
+            $response = new Response();
+            $response->headers->set('Content-Type', 'text/plain');
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+            $response->setContent($e->getMessage());
+        }
+        */
 
-		$response->send();
-	}
+        if ($this['x-frame-options']) {
+            $response->headers->set('x-frame-options', $this['x-frame-options']);
+        }
 
-	/**
-	 * Return the instance of specified model.
-	 *
-	 * @param string $sModel
-	 * @return \Tao\Database\Model
-	 */
-	public function getModel($sModel)
-	{
-		$namespacedClass = $this['database.models_namespace'] . '\\' . $sModel;
+        $response->prepare($this['request']);
 
-		if (!isset(static::$models[$sModel])) {
-			static::$models[$sModel] = new $namespacedClass($this);
-		}
+        $this['triggers']->callTrigger('before-send-response', $this);
 
-		return static::$models[$sModel];
-	}
+        $response->send();
+    }
+
+    protected function controllerResolver()
+    {
+        if (!$controller = $this['request']->attributes->get('_controller')) {
+            throw new \RuntimeException('Unable to look for the controller as the "controller" parameter is missing');
+        }
+
+        if (false === strpos($controller, '::')) {
+            throw new \RuntimeException(sprintf('Unable to find controller "%s".', $controller));
+        }
+
+        list($class, $method) = explode('::', $controller, 2);
+
+        if ($this['routing.controllers_namespace']) {
+            $class = $this['routing.controllers_namespace'] . '\\' . $class;
+        }
+
+        if (!class_exists($class)) {
+            throw new \RuntimeException(sprintf('Class "%s" does not exist.', $class));
+        }
+
+        $callable = [
+            new $class($this),
+            $method
+        ];
+
+        if (!is_callable($callable)) {
+            throw new \RuntimeException(sprintf('The controller for URI "%s" is not callable.', $this['request']->getPathInfo()));
+        }
+
+        $this['request']->attributes->set('controller_class', $class);
+        $this['request']->attributes->set('controller_method', $method);
+
+        return call_user_func($callable);
+    }
+
+    /**
+     * Return the instance of specified model.
+     *
+     * @param string $sModel
+     * @return \Tao\Database\Model
+     */
+    public function getModel($sModel)
+    {
+        $namespacedClass = $this['database.models_namespace'] . '\\' . $sModel;
+
+        if (!isset(static::$models[$sModel])) {
+            static::$models[$sModel] = new $namespacedClass($this);
+        }
+
+        return static::$models[$sModel];
+    }
 }
--- src/Tao/Provider/RouterServiceProvider.php
+++ src/Tao/Provider/RouterServiceProvider.php
@@ -6,87 +6,89 @@ use Pimple\ServiceProviderInterface;
 
 class RouterServiceProvider implements ServiceProviderInterface
 {
-	public function register(Container $app)
-	{
-		$app['requestContext'] = function() use ($app)  {
+    public function register(Container $app)
+    {
+        $app['requestContext'] = function() use ($app)  {
 
-			$requestContext = new $app['class']['routing.request_context'];
-			$requestContext->fromRequest($app['request']);
+            $requestContext = new $app['class']['routing.request_context'];
+            $requestContext->fromRequest($app['request']);
 
-			return $requestContext;
-		};
+            return $requestContext;
+        };
 
-		$app['routing.locator'] = function() use ($app)  {
-			return new $app['class']['routing.locator']($app['routing.resources_dirs']);
-		};
+        $app['routing.locator'] = function() use ($app)  {
+            return new $app['class']['routing.locator']($app['routing.resources_dirs']);
+        };
 
-		$app['route.loader'] = function() use ($app)  {
-			return new $app['class']['routing.loader'](
-				$app['routing.locator']
-			);
-		};
+        $app['route.loader'] = function() use ($app)  {
+            return new $app['class']['routing.loader'](
+                $app['routing.locator']
+            );
+        };
 
-		$app['router'] = function() use ($app)  {
+        $app['router'] = function() use ($app)  {
 
-			$router =  new $app['class']['routing.router'](
-				$app['route.loader'],
-				$app['routing.resource_name'],
-				[
-					'debug' 	=> $app['debug'],
-					'cache_dir' => $app['routing.cache_dir'],
+            $router =  new $app['class']['routing.router'](
+                $app['route.loader'],
+                $app['routing.resource_name'],
+                [
+                    'debug' 	=> $app['debug'],
+                    'cache_dir' => $app['routing.cache_dir'],
 
-					'generator_class' => $app['class']['routing.generator'],
-					'generator_base_class' => $app['class']['routing.generator_base'],
-					'generator_dumper_class' => $app['class']['routing.generator_dumper'],
-					'generator_cache_class' => $app['class']['routing.generator_cache'] ?: $app['app_id'] . 'UrlGenerator',
+                    'generator_class' => $app['class']['routing.generator'],
+                    'generator_base_class' => $app['class']['routing.generator_base'],
+                    'generator_dumper_class' => $app['class']['routing.generator_dumper'],
+                    'generator_cache_class' => $app['class']['routing.generator_cache'] ?: $app['app_id'] . 'UrlGenerator',
 
-					'matcher_class' => $app['class']['routing.matcher'],
-					'matcher_base_class' => $app['class']['routing.matcher_base'],
-					'matcher_dumper_class' => $app['class']['routing.matcher_dumper'],
-					'matcher_cache_class' => $app['class']['routing.matcher_cache'] ?: $app['app_id'] . 'UrlMatcher',
+                    'matcher_class' => $app['class']['routing.matcher'],
+                    'matcher_base_class' => $app['class']['routing.matcher_base'],
+                    'matcher_dumper_class' => $app['class']['routing.matcher_dumper'],
+                    'matcher_cache_class' => $app['class']['routing.matcher_cache'] ?: $app['app_id'] . 'UrlMatcher',
 
-					'resource_type' => $app['routing.resource_type'],
-					'strict_requirements' => $app['routing.strict_requirements']
-				],
-				$app['requestContext']
-			);
+                    'resource_type' => $app['routing.resource_type'],
+                    'strict_requirements' => $app['routing.strict_requirements']
+                ],
+                $app['requestContext']
+            );
 
-			return $router;
-		};
+            return $router;
+        };
 
-		$app['controllerResolver'] = function() use ($app) {
+        /*
+        $app['controllerResolver'] = function() use ($app) {
 
-			if (!$controller = $app['request']->attributes->get('_controller')) {
-				throw new \RuntimeException('Unable to look for the controller as the "controller" parameter is missing');
-			}
+            if (!$controller = $app['request']->attributes->get('_controller')) {
+                throw new \RuntimeException('Unable to look for the controller as the "controller" parameter is missing');
+            }
 
-			if (false === strpos($controller, '::')) {
-				throw new \RuntimeException(sprintf('Unable to find controller "%s".', $controller));
-			}
+            if (false === strpos($controller, '::')) {
+                throw new \RuntimeException(sprintf('Unable to find controller "%s".', $controller));
+            }
 
-			list($class, $method) = explode('::', $controller, 2);
+            list($class, $method) = explode('::', $controller, 2);
 
-			if ($app['routing.controllers_namespace']) {
-				$class = $app['routing.controllers_namespace'] . '\\' . $class;
-			}
+            if ($app['routing.controllers_namespace']) {
+                $class = $app['routing.controllers_namespace'] . '\\' . $class;
+            }
 
-			if (!class_exists($class)) {
-				throw new \RuntimeException(sprintf('Class "%s" does not exist.', $class));
-			}
+            if (!class_exists($class)) {
+                throw new \RuntimeException(sprintf('Class "%s" does not exist.', $class));
+            }
 
-			$callable = [
-				new $class($app),
-				$method
-			];
+            $callable = [
+                new $class($app),
+                $method
+            ];
 
-			if (!is_callable($callable)) {
-				throw new \RuntimeException(sprintf('The controller for URI "%s" is not callable.', $app['request']->getPathInfo()));
-			}
+            if (!is_callable($callable)) {
+                throw new \RuntimeException(sprintf('The controller for URI "%s" is not callable.', $app['request']->getPathInfo()));
+            }
 
-			$app['request']->attributes->set('controller_class', $class);
-			$app['request']->attributes->set('controller_method', $method);
+            $app['request']->attributes->set('controller_class', $class);
+            $app['request']->attributes->set('controller_method', $method);
 
-			return call_user_func($callable);
-		};
-	}
+            return call_user_func($callable);
+        };
+        */
+    }
 }
